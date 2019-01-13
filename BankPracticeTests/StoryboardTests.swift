546bfc71d81bad089d27dd6275ce991ce31a5fee
Drew Sullivan <drew.sullivan.dma@gmail.com>
2019-01-13 08:50:21 -0500

    Added storyboard tests

---
+++ BankPracticeTests/StoryboardTests.swift
@@ -0,0 +1,31 @@
+//
+//  StoryboardTests.swift
+//  BankPracticeTests
+//
+//  Created by Drew Sullivan on 1/13/19.
+//  Copyright © 2019 Drew Sullivan, DMA. All rights reserved.
+//
+
+import XCTest
+
+@testable import BankPractice
+
+class StoryboardTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_initial_view_controller_is_bank_table_view_controller() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let rootViewController = navigationController.viewControllers[0]
+        
+        XCTAssertTrue(rootViewController is BankListViewController)
+    }
+
+}
